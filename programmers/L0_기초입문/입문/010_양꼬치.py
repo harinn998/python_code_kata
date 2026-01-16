832877b0288a7791fc6cdb835919ad8f94c77573
harinn998 <156496765+harinn998@users.noreply.github.com>
2026-01-16 15:43:45 +0900

    [프로그래머스] L0 - 양꼬치 (#10)

--- programmers/L0_기초입문/입문/010_양꼬치.py
+++ programmers/L0_기초입문/입문/010_양꼬치.py
@@ -3,11 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120830
 # 알고리즘: 기초
 # 작성자: 박하린
-# 작성일: 2026. 01. 16. 15:39:03
+# 작성일: 2026. 01. 16. 15:43:44
 
 def solution(n, k):
-    if n >= 10:
-        total = 12000*n + 2000*(k-((n)//10))
-    else: 
-        total = 12000*n + 2000*k
-    return total
+    return (12000 * n) + (2000 * (k - (n // 10)))

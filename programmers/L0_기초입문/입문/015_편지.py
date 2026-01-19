ded09b0cf1eef3124ceddfd80320fd31368a7f43
harinn998 <156496765+harinn998@users.noreply.github.com>
2026-01-19 13:50:07 +0900

    [프로그래머스] L0 - 편지 (#15)

---
+++ programmers/L0_기초입문/입문/015_편지.py
@@ -0,0 +1,9 @@
+# 편지
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120898
+# 알고리즘: 기초
+# 작성자: 박하린
+# 작성일: 2026. 01. 19. 13:50:06
+
+def solution(message):
+    return len(message) * 2

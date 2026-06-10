9212efc24f079cc086f343bdaca964d9c84bd4da
harinn998 <156496765+harinn998@users.noreply.github.com>
2026-06-10 16:59:15 +0900

    [프로그래머스] L0 - 인덱스 바꾸기 (#49)

--- programmers/L0_기초입문/입문/049_인덱스_바꾸기.py
+++ programmers/L0_기초입문/입문/049_인덱스_바꾸기.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120895
 # 알고리즘: 기초
 # 작성자: 박하린
-# 작성일: 2026. 06. 10. 16:59:03
+# 작성일: 2026. 06. 10. 16:59:14
 
 def solution(my_string, num1, num2):
     # 수정하기 위해 리스트로 변환 
